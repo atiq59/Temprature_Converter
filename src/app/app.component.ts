1ff9df0fed6c17161c6d00a5de9827a004102ed5
atiq59 <atiq011097@gmail.com>
2023-05-21 14:49:31 +0530

    Added functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'temp_converter';
+	title = "Temprature Converter";
+	degreesControl = new FormControl("32");
+	typeControl = new FormControl("F");
+	results: String = "";
+
+	onClick() {
+
+		if ( (this.degreesControl.value)?.trim().length == 0 || Number.isNaN( Number( this.degreesControl.value ) ) ) {
+
+			window.alert( "Please add only numeric Value" );
+			this.results = "N/A";
+
+			setTimeout(() => {
+				this.degreesControl.reset();
+			}, 100);
+
+		} else {
+
+			switch( this.typeControl.value ) {
+
+				case 'C': 
+					this.results = String(((9 / 5) * Number(this.degreesControl.value) + 32).toFixed(1)) + "ºF";
+					break;
+				
+				case 'F': 
+					this.results = String(((Number(this.degreesControl.value) - 32) * (5 / 9)).toFixed(1)) + "ºC";
+					break;
+
+				default:
+					break;
+
+			}
+
+		}
+
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
